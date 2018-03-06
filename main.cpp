2042cbadc30ce1f2bb4cdc48f20099d8173b432b
siiscil <siiscil@hotmail.fr>
2018-03-06 14:34:01 +0100

    TheBeginning

--- main.cpp
+++ main.cpp
@@ -1,6 +1,35 @@
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+using namespace std;
+
+int main()
+{
+    int niveau;
+    bool toExit = true;
+    cout << "Hello world!" << endl;
+    cout << "Projet de Theorie des Graphes" << endl << endl;
+
+    do{
+        cout <<"Choose your level :"<<endl;
+        cout <<"0)Exit" << endl << "1)Level 1" << endl;
+        cin >> niveau;
+
+        switch(niveau){
+        case 0:{
+            cout << "Exit" << endl;
+            toExit = false;
+            break;
+        }
+        case 1:{
+            cout << "Hello" << endl;
+            break;
+            }
+        default:{
+                cout << "Please choose a good number" << endl;
+                break;
+            }
+        }
+    }
+    while(toExit);
     return 0;
 }

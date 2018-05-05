b7c555c14cbf997fad8d2af48d7d12e876ff081c
WeaselMercury <x.nomicisio@gmail.com>
2018-05-05 15:56:18 +0200

    suppression code corrompu

--- Graphe.h
+++ Graphe.h
@@ -12,11 +12,7 @@
 #include <limits>
 #include <cmath>
 
-<<<<<<< HEAD
-=======
 
-
->>>>>>> 5cdf1cf06e800bed9a53b772a742223e53e3b5e9
 
 class Graphe {
 
@@ -43,12 +39,12 @@ public:
 
 typedef struct sommetListePredecesseurs { /*liste des prédecesseurs associés à un sommet*/
  int sommet ;
- sommetListePredecesseurs * predecesseurSuivant } sommetListePredecesseurs ;
+ sommetListePredecesseurs * predecesseurSuivant; } sommetListePredecesseurs ;
 
  typedef struct listeSommets { /*liste des listes de prédecesseurs = liste de sommets*/
  int sommet ;
  sommetListePredecesseurs * predecesseurs ;
- listeSommets * sommetSuivant } listeSommets ;
+ listeSommets * sommetSuivant; } listeSommets ;
 
 
 #endif //A10THEORIEGRAPHEL3S2_GRAPHE_H
